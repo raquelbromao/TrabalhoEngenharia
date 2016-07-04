f11adbb323db535398f820c8a1a7d279b4a43623
raquelbromao <raquelbromao@gmail.com>
2016-07-04 06:55:08 -0300

    Versão 1.1
    Signed-off-by: raquelbromao <raquelbromao@gmail.com>

--- ControleDeVersao/src/ControleDeVersao.java
+++ ControleDeVersao/src/ControleDeVersao.java
@@ -2,18 +2,34 @@
 public class ControleDeVersao {
 	private int A1;
 	private float A2;
+	String A3;
 	
 	public int getA1() {
 		return A1;
 	}
+	
 	public void setA1(int a1) {
 		A1 = a1;
 	}
+	
 	public float getA2() {
 		return A2;
 	}
+	
 	public void setA2(float a2) {
 		A2 = a2;
 	}	
+
+	public String getA3() {
+		return A3;
+	}
 	
+	public void setA3(String a3) {
+		A3 = a3;
+	}
+	
+	public float soma(int n, float m) {
+		float soma = n + m;
+		return soma;
+	}
 }
