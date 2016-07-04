282bd3608766ce19fb94299d130b96d606e94f45
raquelbromao <raquelbromao@gmail.com>
2016-07-04 09:21:40 -0300

    Adicionada String B4
    Signed-off-by: raquelbromao <raquelbromao@gmail.com>

--- ControleDeVersao/src/ControleB.java
+++ ControleDeVersao/src/ControleB.java
@@ -1,17 +1,28 @@
 public class ControleB {
 	private int B1;
 	private float B2;
+	String B4;
 	
 	public int getB1() {
 		return B1;
 	}
+	
 	public void setB1(int b1) {
 		B1 = b1;
 	}
+	
 	public float getB2() {
 		return B2;
 	}
+	
 	public void setB2(float b2) {
 		B2 = b2;
+	}
+	
+	public String getB4() {
+		return B4;
+	}
+	public void setB4(String b4) {
+		B4 = b4;
 	}	
 }
